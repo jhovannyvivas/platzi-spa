6d1009942579d34968d76093367686a9950a93d4
Jhovanny Vivas <edwin.vivas@alteru.co>
2021-06-30 19:54:05 -0500

    spa completada en mi computador

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('hi')
+console.log('hello');
+import router from './routes';
+
+window.addEventListener('load', router);
+window.addEventListener('hashchange',router);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -27,14 +30,20 @@ module.exports = {
     
     plugins: [
         new HtmlWebpackPlugin(
-            [
+            
                 {
                     inject: true,
                     template: './public/index.html',
                     filename: './index.html'
                 }
-            ]
-        )
+            
+        ),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: './src/styles/styles.css',
+                to: '',
+            }]
+        })
     ]
 
 }
