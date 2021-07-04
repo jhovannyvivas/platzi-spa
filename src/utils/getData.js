78017366b37f798bacbe38f001221ccc3e143fb0
Jhovanny Vivas <edwin.vivas@alteru.co>
2021-07-04 17:56:39 -0500

    tarea finalizada

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,6 +1,7 @@
 const API = 'https://rickandmortyapi.com/api/character/';
 
 const getData = async (id) => {
+
     const apiURL = id ? `${API}${id}` : API;
     try {
         const response = await fetch(apiURL);
--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -1,5 +1,5 @@
 const getHash = () => 
 location.hash.slice(1).toLocaleLowerCase().split("/")[1] || "/";
-
+console.log(getHash);
 export default getHash;
 
